ffdff882ed21d423901d8e6aa91e79d3f680c798
Luara Lima <luaralima205@gmail.com>
2023-08-13 16:55:22 -0300

    documentation

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from transactions import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('incomes/', views.IncomeList.as_view(), name='income-list'),
     path('incomes/<int:pk>/', views.IncomeDetail.as_view(), name='income-detail'),
     path('expenses/', views.ExpenseList.as_view(), name='expense-list'),
