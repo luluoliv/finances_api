b2fa1c48544c3f64d4b57be73893aa231e34c507
Luara Lima <luaralima205@gmail.com>
2023-08-13 20:41:01 -0300

    documentation

--- finances_manager/settings.py
+++ finances_manager/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
