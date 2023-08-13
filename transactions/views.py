fd60b7708892405eb106b716c9aa03240c211bfa
Luara Lima <luaralima205@gmail.com>
2023-08-13 14:34:31 -0300

    new configurations

--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from models import Income, Expense
+from .models import Income, Expense
 
 def index(request):
     return render(request, 'index.html')
